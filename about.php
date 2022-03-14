301d44057baaf31a500283d3973e235889475ac6
Drikshnemkul <drikshnanem@gmail.com>
2022-03-15 04:19:27 +0545

    content added

--- about.php
+++ about.php
@@ -28,6 +28,13 @@
                     <img src="image/cart.png" width="30px"height="30px" alt="">
                 </div>
             </div>
+            <h4>ABOUT US</h4>
+             <p>
+                 We are online vegetable & fruits store. We deliver fredh vegetables and fruits . <br> 
+                 You can buy our produt, add to cart fill your address for delivery. 
+                We aim to change the traditionally way of buying vegetables and fruits. <br> You don't have to waste your time on buying fruits and vegetables to a local markets <br>
+                bargining with shopkeepers and bring your bag.With us you can select the product you wand and buy online.    
+            </p>
         </div>
     
 </body>
